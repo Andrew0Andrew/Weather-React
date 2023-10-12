bb41f0000ebfc91e3394b12e4138f6496523e1fb
Andrew0Andrew <software.engineer.andrew@gmail.com>
2023-10-12 23:49:56 +0400

    fix

--- Weather React js/src/components/weatherapp/WeatherApp.jsx
+++ Weather React js/src/components/weatherapp/WeatherApp.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './WeatherApp.css'
+import { useState } from 'react'
 
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -10,50 +11,58 @@ import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 import humidity_icon from '../assets/humidity.png'
 
-
-
 const WeatherApp = () => {
 
-    let apiKey = '59dd08c581624ef744a15f2c04b9e48e';
+    const [city,setCity]=useState('')
+    const [result,setResult]=useState(null)
 
-    const search = () => {
-        const element = document.getElementsByClassName("city_input")
-        if(element[0].value===''){
-            return 0;
-        }
-        let url =`https://api.openweathermap.org/data/2.5/weather?&units=metric&q=${element[0].value}` 
+    const submit = () => {
+        const apiKey='59dd08c581624ef744a15f2c04b9e48e'
+        const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${apiKey}`
+        fetch(url)
+            .then((response)=>{
+                return response.json()
+            })
+            .then((data)=>{
+                console.log(data)
+                setResult(data)
+            })
     }
 
   return (
     <div className="container">
         <div className="top_bar">
-            <input type="text" className='city_input' placeholder='search'/>
-            <div className="search_icon" onClick={()=>{search()}}>
+            <input type="text" className='city_input' placeholder='search' value={city} onChange={e => setCity(e.target.value)}/>
+            <div className="search_icon" onClick={submit}>
                 <img src={search_icon} className='search_icon_pic'/>
             </div>
         </div>
+        {result && (
+        <>
         <div className="weather_image">
             <img src={clouds_icon}/>
         </div>
-        <div className="weather_temp">24°C</div>
-        <div className="weather_location">London</div>
+        <div className="weather_temp">{Math.round(result.main.temp)}°C</div>
+        <div className="weather_location">{result.name}</div>
 
         <div className="data_container">
             <div className="element">
                 <img src={humidity_icon} className='icon'/>
                 <div className="data">
-                    <div className="humidity_percent">64%</div>
+                    <div className="humidity_percent">{result.main.humidity}%</div>
                     <div className="text">Humidity</div>
                 </div>
             </div>
             <div className="element">
                 <img src={wind_icon} className='icon'/>
                 <div className="data">
-                    <div className="humidity_percent">18 km/h</div>
+                    <div className="wind_speed">{Math.round(result.wind.speed)} km/h</div>
                     <div className="text">Wind Speed</div>
                 </div>
             </div>
         </div>
+        </>
+        )}
     </div>
   )
 }
